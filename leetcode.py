1d76898dbac0c1e62701a4612dc29d34e4868143
Nairod <dorian.ayoul@epitech.eu>
2024-02-19 10:49:18 +0100

    feat: trap

--- leetcode.py
+++ leetcode.py
@@ -178,3 +178,22 @@ class Solution(object):
             else: right -= 1
             if best_area > max_height * (right - left): break
         return best_area
+    # https://leetcode.com/problems/trapping-rain-water/
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        # at each step, converge left and right while adding their max and current height substraction in the total
+        left, right, total = 0, len(height) - 1, 0
+        left_max, right_max = height[left], height[right]
+        while left < right:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                total += (left_max - height[left])
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                total += (right_max - height[right])
+        return total
