aa2c6b0512ef2555be6375e5e4b111f4cf4f3e7c
Nairod <dorian.ayoul@epitech.eu>
2024-09-22 16:59:24 +0200

    feat: partition

--- leetcode.py
+++ leetcode.py
@@ -943,3 +943,16 @@ class Solution(object):
             board[row][col] = temp
             return found
         return any(backtrack(row, col, 0) for row in range(rows) for col in range(cols))
+    # https://leetcode.com/problems/palindrome-partitioning/
+    def partition(self, s: str) -> List[List[str]]:
+        # backtrack through s until its end while saving path when current substring is equal to invert (palindrome)
+        def backtrack(start, path):
+            if start == len(s):
+                result.append(path)
+                return
+            for end in range(start+1, len(s)+1):
+                if s[start:end] == s[start:end][::-1]:
+                    backtrack(end, path + [s[start:end]])
+        result = []
+        backtrack(0, [])
+        return result
