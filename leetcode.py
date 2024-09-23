7cdcbdb55aebe3c58ddc7962f546cf9f4869d165
Nairod <dorian.ayoul@epitech.eu>
2024-09-23 22:07:09 +0200

    feat: solveNQueens

--- leetcode.py
+++ leetcode.py
@@ -973,3 +973,27 @@ class Solution(object):
         combinations = []
         backtrack(0, "")
         return combinations
+    # https://leetcode.com/problems/n-queens/
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        # backtrack each row trying to place a queen in every column while checking for invalid spots with other queens' cover sets until we reach the last row
+        def backtrack(row):
+            if row == n:
+                result.append(["".join(board[r]) for r in range(n)])
+                return
+            for col in range(n):
+                if col in cols or (row - col) in diag1 or (row + col) in diag2:
+                    continue
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+                board[row][col] = 'Q'
+                backtrack(row + 1)
+                board[row][col] = '.'
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
+        result = []
+        cols, diag1, diag2 = set(), set(), set()
+        board = [['.'] * n for _ in range(n)]
+        backtrack(0)
+        return result
