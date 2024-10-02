008c9750ac3147704295d98fd430b0a0251c9f52
Nairod <dorian.ayoul@epitech.eu>
2024-10-02 17:56:40 +0200

    feat: maxAreaOfIsland

--- leetcode.py
+++ leetcode.py
@@ -1134,3 +1134,17 @@ class Solution(object):
                     island_count += 1
                     DFS(i, j)
         return island_count
+    # https://leetcode.com/problems/max-area-of-island/
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        # like numIslands() but DFS returns 1 (current) + the area sum of all 4 directions and keep track of the max_area to return it
+        if not grid: return 0
+        def DFS(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0: return 0
+            grid[i][j] = 0
+            return 1 + DFS(i - 1, j) + DFS(i + 1, j) + DFS(i, j - 1) + DFS(i, j + 1)
+        max_area = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    max_area = max(max_area, DFS(i, j))
+        return max_area
