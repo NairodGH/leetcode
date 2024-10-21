f013d184fdc2dbb65c2ce42e5ed6f7bfce3d6b95
Nairod <dorian.ayoul@epitech.eu>
2024-10-21 23:30:19 +0200

    feat: maxProduct

--- leetcode.py
+++ leetcode.py
@@ -1648,3 +1648,19 @@ class Solution(object):
             for i in range(coin, amount + 1):
                 dp[i] = min(dp[i], dp[i - coin] + 1)
         return dp[amount] if dp[amount] != float('inf') else -1
+    # https://leetcode.com/problems/maximum-product-subarray/
+    def maxProduct(self, nums: List[int]) -> int:
+        # simple store of the max product along nums except we also store the min to swap when negative so that we keep actual max/min products
+        if not nums:
+            return 0
+        max_product = nums[0]
+        min_product = nums[0]
+        result = nums[0]
+        for i in range(1, len(nums)):
+            num = nums[i]
+            if num < 0:
+                max_product, min_product = min_product, max_product
+            max_product = max(num, max_product * num)
+            min_product = min(num, min_product * num)
+            result = max(result, max_product)
+        return result
