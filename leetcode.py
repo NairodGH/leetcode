f73297c8e553081a637a14c7d1f4c274aed2586a
Nairod <dorian.ayoul@epitech.eu>
2024-10-24 23:19:05 +0200

    feat: canPartition

--- leetcode.py
+++ leetcode.py
@@ -1688,3 +1688,16 @@ class Solution(object):
             else:
                 sub.append(num)
         return len(sub)
+    # https://leetcode.com/problems/partition-equal-subset-sum/
+    def canPartition(self, nums: List[int]) -> bool:
+        # odd total=not possible, update (backward to avoid reusing numbers) target-long falses array where dp[i]=subset with sum i exists so true dp[target]=good
+        total_sum = sum(nums)
+        if total_sum % 2 != 0:
+            return False
+        target = total_sum // 2
+        dp = [False] * (target + 1)
+        dp[0] = True
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+        return dp[target]
