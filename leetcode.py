795694222776cb0bd61dfcf2610dec699dfafb88
Nairod <dorian.ayoul@epitech.eu>
2024-11-01 16:28:46 +0100

    feat: jump

--- leetcode.py
+++ leetcode.py
@@ -1802,3 +1802,17 @@ class Solution(object):
             else:
                 require += 1
         return not require
+    # https://leetcode.com/problems/jump-game-ii/
+    def jump(self, nums: List[int]) -> int:
+        # iterate nums while saving each jump's end and the current farthest reach until we reach the end, return the jumps count incremented at each jump's end
+        jumps = 0
+        current_end = 0
+        farthest = 0
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+                if current_end >= len(nums) - 1:
+                    break
+        return jumps
