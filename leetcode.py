e83a9f4dab7fe340b2386711410fce0036746962
Nairod <dorian.ayoul@epitech.eu>
2024-11-17 19:14:26 +0100

    feat: minDistance

--- leetcode.py
+++ leetcode.py
@@ -2110,3 +2110,23 @@ class Solution(object):
                 else:
                     dp[i][j] = dp[i - 1][j]
         return dp[m][n]
+    # https://leetcode.com/problems/edit-distance/
+    def minDistance(self, word1: str, word2: str) -> int:
+        # dp[i][j] = min operations to convert word1[:i] into word2[:j], for each word1/word2 chars if diff char pick min cost else skip, result is last
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][0] = i
+        for j in range(n + 1):
+            dp[0][j] = j
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(
+                        dp[i][j - 1],  # Insert
+                        dp[i - 1][j],  # Delete
+                        dp[i - 1][j - 1]  # Replace
+                    ) + 1
+        return dp[m][n]
