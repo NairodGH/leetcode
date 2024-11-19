f15c4a64921c8f9d5e467a0b9c1b3b21f7275e35
Nairod <dorian.ayoul@epitech.eu>
2024-11-19 22:18:12 +0100

    feat: isMatch

--- leetcode.py
+++ leetcode.py
@@ -2145,3 +2145,19 @@ class Solution(object):
                         dp[left][lastBurst-1] + nums[left-1] * nums[lastBurst] * nums[right+1] + dp[lastBurst+1][right]
                     )
         return dp[1][n-2]
+    # https://leetcode.com/problems/regular-expression-matching/
+    def isMatch(self, s: str, p: str) -> bool:
+        # dp[i][j]=does s[:i] match p[:j], init empty string patterns, for each chars fill dp based on same chars/"." or "*", result is last (entire string match)
+        m, n = len(s), len(p)
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
+        for j in range(2, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '.' or p[j - 1] == s[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 2] or (dp[i - 1][j] and (p[j - 2] == '.' or p[j - 2] == s[i - 1]))
+        return dp[m][n]
