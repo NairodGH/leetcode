f1080a3149413a28e4d0c2803d0107ff2889928c
Nairod <dorian.ayoul@epitech.eu>
2024-11-28 16:59:11 +0100

    feat: myPow (or just x**n ...)

--- leetcode.py
+++ leetcode.py
@@ -2281,3 +2281,18 @@ class Solution(object):
                 return digits
             digits[i] = 0
         return [1] + digits
+    # https://leetcode.com/problems/powx-n/
+    def myPow(self, x: float, n: int) -> float:
+        # Divide and Conquer using formulas: xⁿ⁽ᵉᵛᵉⁿ⁾=(xⁿᐟ²)², xⁿ⁽ᵒᵈᵈ⁾=x*xⁿ⁻¹, x⁻ⁿ=1/xⁿ
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n = -n
+        result = 1
+        while n != 0:
+            if n % 2 == 1:
+                result *= x
+            x *= x
+            n //= 2
+        return result
