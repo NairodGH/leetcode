3f48ddb081f12db066dae3c5c51c6c7769a4755a
Nairod <dorian.ayoul@epitech.eu>
2024-11-28 17:17:46 +0100

    feat: multiply

--- leetcode.py
+++ leetcode.py
@@ -2296,3 +2296,20 @@ class Solution(object):
             x *= x
             n //= 2
         return result
+    # https://leetcode.com/problems/multiply-strings/
+    def multiply(self, num1: str, num2: str) -> str:
+        # init big enough result array, easier to reverse before/after, sum multiplication of each num1 digit by each num2 digit with carry overs, remove leading 0s
+        if num1 == "0" or num2 == "0":
+            return "0"
+        m, n = len(num1), len(num2)
+        result = [0] * (m + n)
+        num1, num2 = num1[::-1], num2[::-1]
+        for i in range(m):
+            for j in range(n):
+                product = int(num1[i]) * int(num2[j])
+                result[i + j] += product
+                result[i + j + 1] += result[i + j] // 10
+                result[i + j] %= 10
+        while result[-1] == 0:
+            result.pop()
+        return ''.join(map(str, result[::-1]))
