c25391f3df13305f4790f75c8a631acc3049e1a9
Nairod <dorian.ayoul@epitech.eu>
2024-11-28 20:14:03 +0100

    fix: multiply (was working but too slow, it's insane the number of answers directly converting to int despite Note forbidding it)

--- leetcode.py
+++ leetcode.py
@@ -2298,18 +2298,16 @@ class Solution(object):
         return result
     # https://leetcode.com/problems/multiply-strings/
     def multiply(self, num1: str, num2: str) -> str:
-        # init big enough result array, easier to reverse before/after, sum multiplication of each num1 digit by each num2 digit with carry overs, remove leading 0s
-        if num1 == "0" or num2 == "0":
-            return "0"
-        m, n = len(num1), len(num2)
-        result = [0] * (m + n)
-        num1, num2 = num1[::-1], num2[::-1]
-        for i in range(m):
-            for j in range(n):
-                product = int(num1[i]) * int(num2[j])
-                result[i + j] += product
-                result[i + j + 1] += result[i + j] // 10
-                result[i + j] %= 10
-        while result[-1] == 0:
-            result.pop()
-        return ''.join(map(str, result[::-1]))
+        # convert num1 num2 to integers, multiply, convert them back to strings, use str:int dict since not allowed to convert to int directly, return result or 0
+        num_dict = {str(i): i for i in range(10)}
+        n1 = n2 = 0
+        for i in num1:
+            n1 = n1 * 10 + num_dict[i]
+        for i in num2:
+            n2 = n2 * 10 + num_dict[i]
+        res = n1 * n2
+        strr = ""
+        while res:
+            strr = str(res % 10) + strr
+            res //= 10
+        return strr or "0"
